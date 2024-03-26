5f5af29f7ad25fa75d5dd98daaf9416e1bb2b2ee
nikhil16kulkarni <nikhil16kulkarni@gmail.com>
2024-03-25 22:31:48 -0400

    model training with 10 epochs, ss taken, h5 and keras model issue resolved

---
+++ src/ChickenDiseaseClassification/components/training.py
@@ -0,0 +1,101 @@
+import os
+import urllib.request as request
+from zipfile import ZipFile
+import tensorflow as tf
+import time
+from pathlib import Path
+from ChickenDiseaseClassification.entity.config_entity import *
+
+class Training:
+    def __init__(self, config: TrainingConfig):
+        self.config = config
+
+    def get_base_model(self):
+        self.model = tf.keras.models.load_model(
+            self.config.updated_base_model_path
+        )
+
+    def train_valid_generator(self):
+
+        datagenerator_kwargs = dict(
+            rescale = 1./255,
+            validation_split=0.20
+        )
+
+        dataflow_kwargs = dict(
+            target_size=self.config.params_image_size[:-1],
+            batch_size=self.config.params_batch_size,
+            interpolation="bilinear"
+        )
+
+        valid_datagenerator = tf.keras.preprocessing.image.ImageDataGenerator(
+            **datagenerator_kwargs
+        )
+
+        self.valid_generator = valid_datagenerator.flow_from_directory(
+            directory=self.config.training_data,
+            subset="validation",
+            shuffle=False,
+            **dataflow_kwargs
+        )
+
+        if self.config.params_is_augmentation:
+            train_datagenerator = tf.keras.preprocessing.image.ImageDataGenerator(
+                rotation_range=40,
+                horizontal_flip=True,
+                width_shift_range=0.2,
+                height_shift_range=0.2,
+                shear_range=0.2,
+                zoom_range=0.2,
+                **datagenerator_kwargs
+            )
+        else:
+            train_datagenerator = valid_datagenerator
+
+        self.train_generator = train_datagenerator.flow_from_directory(
+            directory=self.config.training_data,
+            subset="training",
+            shuffle=True,
+            **dataflow_kwargs
+        )
+
+    @staticmethod
+    def save_model(path: Path, model: tf.keras.Model):
+        model.save(path)
+
+    def compile_model(self):
+        # Compile the model with the same optimizer and loss function
+        self.model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    def reset_optimizer_state(self):
+        # Reset the optimizer state
+        for layer in self.model.layers:
+            if hasattr(layer, 'optimizer'):
+                for optimizer in layer.optimizer:
+                    optimizer.reset_states()
+
+
+
+    def train(self, callback_list: list):
+        self.steps_per_epoch = self.train_generator.samples // self.train_generator.batch_size
+        self.validation_steps = self.valid_generator.samples // self.valid_generator.batch_size
+
+        # Recompile the model before training
+        self.compile_model()
+
+        # Reset optimizer state
+        self.reset_optimizer_state()
+
+        self.model.fit(
+            self.train_generator,
+            epochs=self.config.params_epochs,
+            steps_per_epoch=self.steps_per_epoch,
+            validation_steps=self.validation_steps,
+            validation_data=self.valid_generator,
+            callbacks=callback_list
+        )
+
+        self.save_model(
+            path=self.config.trained_model_path,
+            model=self.model
+        )
